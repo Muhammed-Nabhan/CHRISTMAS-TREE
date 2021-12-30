9dbff70422eb74140532e31a0bb4760d5b0d7192
Muhammed-Nabhan <83818758+Muhammed-Nabhan@users.noreply.github.com>
2021-12-30 17:26:55 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,23 @@
+import colorama
+from colorama import Fore,Back,Style
+colorama.init(autoreset=True)
+
+height=16
+wood=height//3
+floor=1
+k=1
+
+print(Fore.RED+'###===***THE CHRISTMAS TREE***===###') 
+
+for i in range(height):
+    print((' ' * (height - i)) + ( Fore.GREEN+'*' * ((2 * i) + 1)))
+  
+for i in range(wood):
+     print(('  '*((height-1)//2))+(Fore.MAGENTA+'#' * wood) )
+for i in range(floor):
+    for j in range(k):
+       print(Fore.GREEN+'v '*18)
+       print(Fore.YELLOW+'@@'*18)
+       print(Fore.GREEN+'o0'*18)
+    
+
